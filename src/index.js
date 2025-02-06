0987bb7d66f57c17ceb7782151979085ed5c89a3
Hashim. A <162900023+HashimAyamon@users.noreply.github.com>
2025-02-06 12:20:51 +0530

    Context Created

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import CryptoContext from "./context/CryptoContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CryptoContext>
+      <App />
+    </CryptoContext>
   </React.StrictMode>
 );
-
-
